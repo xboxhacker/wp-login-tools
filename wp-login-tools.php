e1321e11c34fdd88ec8d4041676f54350b5fc8e6
William Hare <billman311@aol.com>
2025-01-09 14:16:39 -0500

    Update wp-login-tools.php

--- wp-login-tools.php
+++ wp-login-tools.php
@@ -1,1 +1,16 @@
+<?php
+/*
+Plugin Name: WP Login Tools
+Description: A plugin to customize the WordPress login page logo.
+Version: 1.0
+Author: xboxhacker
+*/
 
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+require_once plugin_dir_path(__FILE__) . 'includes/settings-page.php';
+require_once plugin_dir_path(__FILE__) . 'includes/media-handler.php';
+require_once plugin_dir_path(__FILE__) . 'includes/logo-replacer.php';
+?>
